4add28a75024ae5569ed5f9e3a246ba5882a8535
eduardoPaz <paz.dudu@gmail.com>
2025-04-08 21:30:16 -0300

    implementando cadastrar pessoas

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
     <h1>Bem-vindo ao Sistema de Cadastro Mobiliário</h1>
     <ul>
-        <li><a href="pessoas/listar.php">Gerenciar Pessoas</a></li>
+        <li><a href="pessoas/cadastrar.php">Cadastrar Pessoa</a></li>
         <li><a href="imoveis/listar.php">Gerenciar Imóveis</a></li>
     </ul>
 </body>
--- pessoas/cadastrar.php
+++ pessoas/cadastrar.php
@@ -3,9 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar Pessoa -></title>
+    <title>Cadastrar Pessoa</title>
 </head>
 <body>
+
+    <form action="salvar.php" method="POST">
+        <label>Nome*: <input Type="text" name="nome_pessoas" required></label><br><br>
+        <label>Data de Nascimento*: <input Type="date" name="data_nascimento" required></label><br><br>
+        <label>CPF*: <input Type="text" name="cpf_pessoas" required></label><br><br>
+        <label>Sexo*:
+            <select name="sexo" required>
+                <option value="">Selecione</option>
+                <option value="M">Masculino</option>
+                <option value="F">Feminino</option>
+                <option value="Outro">Outros sexos</option>
+            </select>
+        </label><br><br>
+        <label>Telefone: <input Type="text" name="telefone" required></label><br><br>
+        <label>Email: <input Type="text" name="email" required></label><br><br>
+
+        <input type="submit" value="Cadastrar" href="../index.php">
+
+    </form>
     
+    <p><a href="../index.php">Voltar</a></p>
+                
+
 </body>
 </html>
